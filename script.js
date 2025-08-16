cd1bb8c94d8d93b0687b44e34672da4c7b98f2a2
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2025-08-16 08:46:56 +0530

    Add the playRound function

--- script.js
+++ script.js
@@ -20,3 +20,22 @@ console.log(getHumanChoice());
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toUpperCase();
+    computerChoice = computerChoice.toUpperCase();
+
+    let humanWins = (
+        (humanChoice === 'ROCK' && computerChoice === 'SCISSORS')
+        || (humanChoice === 'PAPER' && computerChoice === 'ROCK')
+        || (humanChoice === 'SCISSORS' && computerChoice === 'PAPER')
+    );
+
+    if (humanWins) {
+        console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    } else {
+        console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+}
